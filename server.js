b261ee49c70f6779a205be0012cf5c54775e61c5
jjardine85 <jamesjardine@gmx.com>
2019-02-14 13:27:25 +0000

    tested CRUD functions using insomnia

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const parser = require('body-parser');
 const createRouter = require('./helpers/create_router.js');
+
 app.use(parser.json());
 app.use(cors()) ;
 app.get('/', (req, res) => {
@@ -12,11 +13,9 @@ app.get('/', (req, res) => {
 
 MongoClient.connect('mongodb://localhost:27017')
 .then((client)=> {
-  console.log("HELLO WORLD");
   const db = client.db('media');
   const mediaCollection = db.collection('movies');
   const mediaRouter = createRouter(mediaCollection);
-  console.log(mediaRouter);
   app.use('/api/movies', mediaRouter);
 })
 .catch(console.err);
