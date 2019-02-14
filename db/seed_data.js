9c787c34f58eb63f92a45827ba04410bd77badc4
jjardine85 <jamesjardine@gmx.com>
2019-02-14 11:34:27 +0000

    Add images key to db and added links

--- db/seed_data.js
+++ db/seed_data.js
@@ -5,16 +5,19 @@ db.movies.insertMany([
   {
     name: "Lego Batman",
     genre: "Animation",
-    language: "English"
+    language: "English",
+    image: "https://upload.wikimedia.org/wikipedia/en/6/61/The_Lego_Batman_Movie_PromotionalPoster.jpg"
   },
   {
     name: "You've got mail",
     genre: "Romance",
-    language: "English"
+    language: "English",
+    image: "https://images-na.ssl-images-amazon.com/images/I/513FXJJZ5ZL.jpg"
   },
   {
     name: "Oldboy",
     genre: "Action",
-    language: "Korean"
+    language: "Korean",
+    image: "https://nofilmschool.com/sites/default/files/styles/article_wide/public/oldboytragedy2.jpg?itok=jx7Ruvo5"
   }
 ])
